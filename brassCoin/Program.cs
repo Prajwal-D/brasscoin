f38694e91ecfb52c72b4aa72e5c8c7e0d03b70d1
prajw <prajwal.dethekar@gmail.com>
2021-10-29 01:04:46 +0100

    there's a bug that means i need to define a ssl certificate and something oh my god this boggles my mind i sure hope this works

--- brassCoin/Program.cs
+++ brassCoin/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -22,8 +24,19 @@ namespace brassCoin
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    var path = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "brasscoin.pfx");
+
+                    X509Certificate2 certificate = new X509Certificate2(File.ReadAllBytes(path),"hunter2");
+
                     webBuilder.UseUrls("http://0.0.0.0:5000;https://0.0.0.0:5001");
                     webBuilder.UseKestrel();
+                    webBuilder.ConfigureKestrel(serverOptions =>
+                    {
+                        serverOptions.ConfigureHttpsDefaults(listenOptions =>
+                        {
+                            listenOptions.ServerCertificate = certificate;
+                        });
+                    });
                     webBuilder.UseStartup<Startup>();
                 });
     }
